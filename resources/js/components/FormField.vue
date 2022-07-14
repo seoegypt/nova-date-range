4663224fd9c5142ec41c4615f37596ede5381b45
Sam D Harris <samdharris@outlook.com>
2022-07-14 13:52:37 +0100

    Make field work

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,6 +1,6 @@
 <template>
-    <default-field :field="field">
-        <template slot="field">
+    <DefaultField :field="field" :errors="errors" :show-help-text="showHelpText">
+        <template #field>
             <date-range-picker
                 class="w-full form-control form-input form-input-bordered"
                 mode="sa"
@@ -19,7 +19,7 @@
                 {{ firstError }}
             </p>
         </template>
-    </default-field>
+    </DefaultField>
 </template>
 
 <script>
@@ -28,9 +28,24 @@ import DateRangePicker from './DateRangePicker'
 import { FormField, HandlesValidationErrors } from 'laravel-nova'
 
 export default {
+    props: ['resourceName', 'resourceId', 'field'],
     mixins: [HandlesValidationErrors, FormField],
     components: { DateRangePicker },
+    methods: {
+        /*
+         * Set the initial, internal value for the field.
+         */
+        setInitialValue() {
+            this.value = this.field.value || ''
+        },
 
+        /**
+         * Fill the given FormData object with the field's internal value.
+         */
+        fill(formData) {
+            formData.append(this.field.attribute, this.value || '')
+        },
+    },
     computed: {
         format() {
             return this.field.format
